9e1a1eb0d582b760610757628e0451a5ee67e622
robie01 <robie.sun.rs@gmail.com>
2018-02-27 00:20:24 +0100

    dices and button view implemented

--- app/src/main/java/com/robiesun/rolldicer/MainActivity.java
+++ app/src/main/java/com/robiesun/rolldicer/MainActivity.java
@@ -1,9 +1,23 @@
 package com.robiesun.rolldicer;
 
+import android.annotation.TargetApi;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
