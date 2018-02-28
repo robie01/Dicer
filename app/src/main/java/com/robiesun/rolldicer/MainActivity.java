3d3120055213e041271d6f0c1eb622271ca09020
robie01 <robie.sun.rs@gmail.com>
2018-02-28 10:49:35 +0100

    rolling dice works now , ugly code but works

--- app/src/main/java/com/robiesun/rolldicer/MainActivity.java
+++ app/src/main/java/com/robiesun/rolldicer/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+
 import java.util.Random;
 
 
@@ -14,10 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     public static final Random RANDOM = new Random();
     Button rollDices;
-    ImageView imageView1, imageView2;
+    ImageView imageView1;
+    ImageView imageView2;
 
-
-
+    int rolledNumber;
+    Random r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +30,40 @@ public class MainActivity extends AppCompatActivity {
         imageView1 = (ImageView) findViewById(R.id.imageView1);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
 
+        r = new Random();
+
         rollDices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int value1 = randomDiceValue();
-                int value2 = randomDiceValue();
-
-                int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "package com.robiesun.rolldicer");
-                int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "package com.robiesun.rolldicer");
-
-                imageView1.setImageResource(res1);
-                imageView2.setImageResource(res2);
+                rolledNumber = r.nextInt(6) + 1;
+                if(rolledNumber == 1){
+                    imageView1.setImageResource(R.drawable.dice_1);
+                    imageView2.setImageResource(R.drawable.dice_1);
+                }else if(rolledNumber == 2){
+                    imageView1.setImageResource(R.drawable.dice_2);
+                    imageView2.setImageResource(R.drawable.dice_2);
+                }else if(rolledNumber == 3){
+                    imageView1.setImageResource(R.drawable.dice_3);
+                    imageView2.setImageResource(R.drawable.dice_3);
+                }else if(rolledNumber == 4){
+                    imageView1.setImageResource(R.drawable.dice_4);
+                    imageView2.setImageResource(R.drawable.dice_4);
+                }else if(rolledNumber == 5){
+                    imageView1.setImageResource(R.drawable.dice_5);
+                    imageView2.setImageResource(R.drawable.dice_5);
+                }else if(rolledNumber == 6){
+                    imageView1.setImageResource(R.drawable.dice_6);
+                    imageView2.setImageResource(R.drawable.dice_6);
+                }
 
             }
         });
     }
 
 
-    public static int randomDiceValue()
-    {
-
-        return RANDOM.nextInt(6) + 1;
-    }
+//    public static int randomDiceValue()
+//    {
+//
+//        return RANDOM.nextInt(6) + 1;
+//    }
 }
