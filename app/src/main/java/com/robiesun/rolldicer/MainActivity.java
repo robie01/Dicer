7150708a1b1904c87248fdb8b369fe7cbeb91d06
robie01 <robie.sun.rs@gmail.com>
2018-02-28 10:07:56 +0100

    some error fix about ContentDescription and is set to null

--- app/src/main/java/com/robiesun/rolldicer/MainActivity.java
+++ app/src/main/java/com/robiesun/rolldicer/MainActivity.java
@@ -1,27 +1,53 @@
 package com.robiesun.rolldicer;
 
-import android.annotation.TargetApi;
-import android.media.AudioAttributes;
-import android.media.SoundPool;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final Random RANDOM = new Random();
+    Button rollDices;
+    ImageView imageView1, imageView2;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        rollDices = (Button) findViewById(R.id.rollDices);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        rollDices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value1 = randomDiceValue();
+                int value2 = randomDiceValue();
+
+                int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "package com.robiesun.rolldicer");
+                int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "package com.robiesun.rolldicer");
+
+                imageView1.setImageResource(res1);
+                imageView2.setImageResource(res2);
+
+            }
+        });
+    }
+
+
+    public static int randomDiceValue()
+    {
+
+        return RANDOM.nextInt(6) + 1;
     }
 }
